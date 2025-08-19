00f8ca84941bea99f0b30ad98de4b0c2b0249121
linwei <ltao0358@uni.sydney.edu.au>
2025-08-19 17:28:21 +1000

    add more details for semantic uncertainty

--- new_code_framework/llm_linguistic_confidence_study/confidence_extraction_methods/semantic_uncertainty.py
+++ new_code_framework/llm_linguistic_confidence_study/confidence_extraction_methods/semantic_uncertainty.py
@@ -3,19 +3,19 @@ class SemanticUncertaintyExtractor():
         self.dataset = dataset
         self.model_name = model_name
     
-    def __call__(self):
+    def __call__(self, dataset, model_name):
         prompt = self.prepare_prompt()
-        responses = self.generate_responses(prompt)
+        responses = self.generate_responses(prompt, dataset, model_name)
         df = self.post_process_responses(responses)
-        return df
+        return df    # return a dataframe with the following columns: question, gold_answer, reponse1, reponse2, reponse3, ..., confidence, accuracy
     
     def prepare_prompt(self):
         pass
     
-    def generate_responses(self, prompt):
+    def generate_responses(self, prompt, dataset, model_name):
         responses = []
-        for i in range(len(self.dataset)):
-            response = self.generate_single_response(prompt, self.model_name)
+        for i in range(len(dataset)):
+            response = self.generate_single_response(prompt, dataset, model_name)
             responses.append(response)
         return responses
     
@@ -23,4 +23,12 @@ class SemanticUncertaintyExtractor():
         pass
     
     def post_process_responses(self, responses):
+        
         pass
+    
+    def calculate_confidence(self, responses, gold_answer):
+        pass
+    
+    def calculate_accuracy(self, responses, gold_answer):
+        pass
+    
