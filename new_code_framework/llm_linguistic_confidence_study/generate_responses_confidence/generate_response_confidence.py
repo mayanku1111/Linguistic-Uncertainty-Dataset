7e53ca6bed53072719b3c6978a9739192776c8a9
linwei <ltao0358@uni.sydney.edu.au>
2025-08-19 14:37:17 +1000

    add new code framework

---
+++ new_code_framework/llm_linguistic_confidence_study/generate_responses_confidence/generate_response_confidence.py
@@ -0,0 +1,12 @@
+def main():
+    # 1. define the dataset
+    # 2. Generate responses
+    # 3. Evaluate the responses
+    # 4. Save the results
+    
+    
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ new_code_framework/llm_linguistic_confidence_study/main.py
@@ -0,0 +1,15 @@
+def main():
+    # 1. define the dataset (simpleqa/mmlupro)
+    # 2. define the model (for example: gpt-5-mini)
+    # 3. define the confidence_extraction_methods and corresponding parameters (prompt, number of samples, etc.)
+    # 4. pass to the confidence extraction method
+    # 5. evaluate the responses with defined metrics
+    # 6. save the results
+    
+    
+    
+    pass
+
+
+if __name__ == "__main__":
+    main()
