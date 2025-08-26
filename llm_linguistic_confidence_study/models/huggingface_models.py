57a16a51cdee9f271eb48c3212de26b767de2434
bokai <bkai0921@uni.sydney.edu.au>
2025-08-27 03:19:00 +1000

    debug lora test

--- llm_linguistic_confidence_study/models/huggingface_models.py
+++ llm_linguistic_confidence_study/models/huggingface_models.py
@@ -9,14 +9,21 @@ class Huggingface():
         self.model_cfg = model_cfg
         self.model, self.tokenizer = self.load_model(self.model_cfg)
         self.generate_config = self.get_generate_config(self.model_cfg)
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
 
     def __call__(self, prompts: list[str], task_name: str = None, batch_job_id: ListConfig | str = None) -> list[str]:
-        inputs = self.tokenizer(prompts, return_tensors="pt", padding=True, truncation=True)
-        outputs = self.model.generate(
-            **inputs,
-            **self.generate_config
-        )
-        responses = self.tokenizer.batch_decode(outputs, skip_special_tokens=True)
+        responses = []
+        batch_size = 4  
+        for i in range(0, len(prompts), batch_size):
+            batch_prompts = prompts[i:i+batch_size]
+            inputs = self.tokenizer(batch_prompts, return_tensors="pt", padding=True, truncation=True).to(self.device)
+            outputs = self.model.generate(
+                **inputs,
+                **self.generate_config,
+                # max_new_tokens=128
+            )
+            responses.extend(self.tokenizer.batch_decode(outputs, skip_special_tokens=True))
+
         return responses
     
     def load_model(self, cfg):
@@ -25,17 +32,17 @@ class Huggingface():
             tokenizer = AutoTokenizer.from_pretrained(cfg.base_model_id)
             model = AutoModelForCausalLM.from_pretrained(
                 cfg.base_model_id,
-                device_map=device
+                device_map=device,
             )
-            model = PeftModel.from_pretrained(model, cfg.lora_path)
+            model = PeftModel.from_pretrained(model, cfg.lora_weight_path)
             model = model.merge_and_unload()
-            model.save_pretrained(cfg.save_path)
-            tokenizer.save_pretrained(cfg.save_path)            
+            # model.save_pretrained(cfg.save_path)
+            # tokenizer.save_pretrained(cfg.save_path)            
         else:
             tokenizer = AutoTokenizer.from_pretrained(cfg.save_path)
             model = AutoModelForCausalLM.from_pretrained(
                 cfg.save_path,
-                device_map=device
+                device_map=device,
             )
         return model, tokenizer
 
@@ -45,7 +52,6 @@ class Huggingface():
             "top_p": cfg.top_p,
             "top_k": cfg.top_k,
             "min_p": cfg.min_p,
-            "thinking": cfg.thinking,
             "max_new_tokens": cfg.max_tokens,
         }
 
